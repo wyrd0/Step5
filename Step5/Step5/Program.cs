507ab3517a1eeb028515bfc85a033851f5db15ac
Mary Olive <mary.olive@me.com>
2016-10-19 09:19:07 -0400

    Index input string

--- Step5/Step5/Program.cs
+++ Step5/Step5/Program.cs
@@ -11,8 +11,20 @@ namespace Step5
         static void Main(string[] args)
         {
             //Get user input into an array
-            Console.WriteLine("Please enter a number:");
+            Console.WriteLine("Please enter a number and press ENTER:     ");
+            string userString = Console.ReadLine();
+            Console.WriteLine(userString);
 
+            //Try to index string
+            int sLength = userString.Length;
+            Console.WriteLine("sLength =" + sLength);
+            foreach (int tempVar in userString)
+            {
+                Console.WriteLine(tempVar);
+            }
+            //Ensure string is a set of integers
+            int[] userNum = new int[userString.Length];
+            //Console.WriteLine(new[]);
             //Manipulate data type, if necessary
             //Display input array in reverse0
         }

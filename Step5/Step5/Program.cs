e124e26775375c95ebdb2d121787a2b225a27741
Mary Olive <mary.olive@me.com>
2016-10-19 08:17:28 -0400

    Add comments outlining initial plan

---
+++ Step5/Step5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input into an array
+            Console.WriteLine("Please enter a number:");
+
+            //Manipulate data type, if necessary
+            //Display input array in reverse0
+        }
+    }
+}
